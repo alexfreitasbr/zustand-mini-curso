42e734d8c4e7eb2592e3b828c916c8399c483c45
alex freitas <alexfreitasbr@hotmail.com>
2026-04-24 14:59:33 -0300

    blackBiear

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,6 +1,12 @@
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
+import { BlackBear } from './cards/blackBearCard';
 
 export const BearPage = () => {
+  const polarBears = useBearStore(state => state.polarBears);
+  const pandaBears = useBearStore(state => state.pandaBears);
+  const increasePolarBears = useBearStore(state => state.increasePolarBears);
+  const increasePandaBears = useBearStore(state => state.increasePandaBears);
   return (
     <>
       <h1>Contador de Osos</h1>
@@ -9,24 +15,15 @@ export const BearPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
+        <BlackBear />
 
         <WhiteCard centered>
           <h2>Osos Polares</h2>
 
           <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
+            <button onClick={() => increasePolarBears(1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+            <button onClick={() => increasePolarBears(-1)}  >-1</button>
 
           </div>
 
@@ -36,9 +33,9 @@ export const BearPage = () => {
           <h2>Osos Pandas</h2>
 
           <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
+            <button onClick={() => increasePandaBears(1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+            <button onClick={() => increasePandaBears(-1)}>-1</button>
           </div>
 
         </WhiteCard>
@@ -50,4 +47,7 @@ export const BearPage = () => {
 
     </>
   );
+
+
 };
+
---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+interface BearState {
+  blackBears: number;
+  polarBears: number;
+  pandaBears: number;
+
+  increaseBlackBears: (by:number) => void;
+  increasePolarBears: (by:number) => void;
+  increasePandaBears: (by:number) => void;
+}
+
+
+export const useBearStore = create<BearState>()((set) => ({
+  blackBears: 16,
+  polarBears: 5,
+  pandaBears: 1,
+
+  increaseBlackBears: (by:number) => set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by:number) => set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by:number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+  removeAllBears: () => set({ blackBears: 0, polarBears: 0, pandaBears: 0 }),
+}))
